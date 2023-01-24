eba6a19424c52fba31e668e42e51cb2dea16ac91
S3li98 <s.ali.uob98@gmail.com>
2023-01-24 19:25:39 +0300

    car show page

--- app/pages/cars/cars.page.ts
+++ app/pages/cars/cars.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Car{
+  name: string;
+  price: number;
+  year: number;
+  img: string;
+  discription: string;
+  like: boolean;
+}
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.page.html',
@@ -10,6 +18,67 @@ export class CarsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cars: Car[] = [
+
+    {
+    name: 'Sonata',
+    price: 3000 ,
+    year:2016,
+    img: 'sonata2016.jpg',
+    discription:'nice',
+    like: true
+    },
+
+    {
+      name: 'Sonata',
+      price: 3000 ,
+      year:2016,
+      img: 'sonata2016.jpg',
+      discription:'nice',
+      like: true
+    },
+
+    {
+        name: 'Sonata',
+        price: 3000 ,
+        year:2016,
+        img: 'sonata2016.jpg',
+        discription:'nice',
+        like: true
+    },
+    {
+      name: 'Sonata',
+      price: 3000 ,
+      year:2016,
+      img: 'sonata2016.jpg',
+      discription:'nice',
+      like: true
+  },
+  {
+    name: 'Sonata',
+    price: 3000 ,
+    year:2016,
+    img: 'sonata2016.jpg',
+    discription:'nice',
+    like: true
+},
+{
+  name: 'Sonata',
+  price: 3000 ,
+  year:2016,
+  img: 'sonata2016.jpg',
+  discription:'nice',
+  like: true
+},
+
+  ];
+
+  likeCar(i:any){
+    // console.log(i);
+    this.cars[i].like = !this.cars[i].like;
+
   }
 
 }
