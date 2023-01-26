d77b2a135ce4dc895721a0b439da7a8553706cfe
S3li98 <s.ali.uob98@gmail.com>
2023-01-26 23:21:39 +0300

    last update in search so far

--- app/pages/cars/cars.page.ts
+++ app/pages/cars/cars.page.ts
@@ -22,9 +22,84 @@ export class CarsPage implements OnInit {
 
   Available_adv = true;
   filter = false;
-
+  car_selected_brand: any;
   first_year:any;
   last_year: any;
+  toyota = false;
+  Nissan = false;
+
+  toyota_cars: any[] = [
+                          "All",
+                          "4Runner",
+                          "86",
+                          "Aurion",
+                          "Avensis",
+                          "Camry",
+                          "Celica",
+                          "Corolla",
+                          "Corona",
+                          "Cressida",
+                          "Crown",
+                          "Echo",
+                          "FJ Cruiser",
+                          "Fortuner",
+                          "Hiace",
+                          "Highlander",
+                          "Hilux",
+                          "IQ",
+                          "Innova",
+                          "Land Cruiser",
+                          "Land Cruser 76",
+                          "Other",
+                          "Prado",
+                          "Previa",
+                          "Prius",
+                          "Rav 4",
+                          "Scion",
+                          "Sequoia",
+                          "Starlet",
+                          "Supra",
+                          "Tacoma",
+                          "Tercel",
+                          "Tundra",
+                          "XA",
+                          "Yaris",
+                          "Zelas",
+                          "Avanza",
+                       ];
+
+    Nissan_cars: any[] = [
+                          "All",
+                          "300ZX",
+                          "350Z",
+                          "370Z",
+                          "Altima",
+                          "Armada",
+                          "Bluebird",
+                          "Datsun",
+                          "GT-R",
+                          "Gloria",
+                          "Juke",
+                          "Livina",
+                          "March",
+                          "Maxima",
+                          "Micra",
+                          "Murano",
+                          "Navara",
+                          "Pathfinder",
+                          "Patrol",
+                          "Qashqai",
+                          "S130",
+                          "Sentra",
+                          "Skyline",
+                          "Sunny",
+                          "Terrano",
+                          "Tiida",
+                          "Titan",
+                          "Van",
+                          "X-Trail",
+                          "Xterra",
+                         ];
 
 
   cars: Car[] = [
@@ -91,6 +166,22 @@ export class CarsPage implements OnInit {
   display_filter(){
     this.filter = !this.filter;
 
+  }
+
+  viewCars(){
+    this.not_show();
+    if(this.car_selected_brand == "toyota"){
+      this.toyota = true;
+    }
+    if(this.car_selected_brand == "Nissan"){
+      this.Nissan = true;
+    }
+    
+  }
+
+  not_show(){
+    this.toyota = false;
+    this.Nissan = false;
   }
 
 }
