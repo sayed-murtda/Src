e53df15474d54b6e9b5ba795d3ac368b0b9351d8
Ali Yusuf <alcndb@gmail.com>
2023-01-27 01:34:32 +0300

    complete showrooms services(showrooms,flatcars)

--- app/pages/all-services/showrooms/showrooms.page.ts
+++ app/pages/all-services/showrooms/showrooms.page.ts
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ShowroomsPage implements OnInit {
 
   constructor() { }
-
+  showrooms =[{
+    name:'Honda Showroom',
+    city:'Muharraq',
+    location:'https://goo.gl/maps/nuE3yZnBUFNHqxETA',
+    number:'177171607',
+    whatsapp:'35053631',
+    instagram:'https://www.instagram.com/honda/?hl=en',
+    logo:'https://www.freeiconspng.com/thumbs/honda-logo-png/honda-logo-png-picture-20.png'
+  },{
+    name:'Miami Cars Showroom',
+    city:'Budayia',
+    location:'https://goo.gl/maps/b1v3ZxcjRPckJU7f6',
+    number:'17734567',
+    whatsapp:'33834044',
+    instagram:'https://www.instagram.com/theshowroom.miami/?hl=en',
+    logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlSvbsnFKVMBFR3H3CmLgKrQebeJ1jvF5q5TJ-NFLICw&s'
+  }]
   ngOnInit() {
   }
 
