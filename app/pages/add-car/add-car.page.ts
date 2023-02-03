4a73955a461922116fa381481a82d06c8c45392a
sayed-murtda <S.murtda@gmail.com>
2023-02-03 20:52:45 +0300

    add camera plug

--- app/pages/add-car/add-car.page.ts
+++ app/pages/add-car/add-car.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Camera} from '@capacitor/camera';
 
 @Component({
   selector: 'app-add-car',
@@ -15,6 +16,29 @@ export class AddCarPage implements OnInit {
 
   back(){
     this.navCtrl.navigateBack("/");
+  }
 
+  img(){
+    this.selectImages();
+  }
+
+  async selectImages() {
+    var options:any={
+      correctOrientation:true,
+      maximumImagesCount : 4,
+      limit:4
+    };
+    Camera.pickImages(options).then((res)=>{
+    var image= res.photos;
+    if(image.length>3)
+    alert('please select 3 only')
+    else
+      for(var i =0;i<3;i++){
+        // this.saveImage(image[i])
+      }
+
+    })
+
+    
   }
 }
