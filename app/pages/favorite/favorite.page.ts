74c24f12d53ce331bd23ec91ba5164ba5f165950
S3li98 <s.ali.uob98@gmail.com>
2023-02-08 19:59:12 +0300

    fav page work

--- app/pages/favorite/favorite.page.ts
+++ app/pages/favorite/favorite.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CarsService } from '../../Service/cars.service';
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.page.html',
   styleUrls: ['./favorite.page.scss'],
 })
 export class FavoritePage implements OnInit {
-
-  constructor() { }
+  Cars:any[]=[];
+  constructor(public CarsSrv:CarsService) {
+    CarsSrv.getFirst10Rows().then((res)=>{
+      if(res){
+      this.Cars=res;
+      console.log(this.Cars);
+      }
+    });
+   }
 
   ngOnInit() {
   }
+
+  Unlike(id:any){
+
+    let i = this.CarsSrv.fav_ID.indexOf(id);
+    this.CarsSrv.fav_ID.splice(i,1);
+    this.CarsSrv.save_unlike();
+ 
+ 
+     
+   }
 
 }
